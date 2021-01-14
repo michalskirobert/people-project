d599cbd9dfeeba56e1263e26dfb5edb06082989e
michalskirobert <majointa@gmail.com>
2021-01-14 19:12:36 +0100

    fixed data

--- src/components/data.js
+++ src/components/data.js
@@ -1,16 +1,19 @@
 export const firstPerson = {
   id: 1,
-  age: 40,
+  age: "40+",
   ethnicity: ["Black", "Hispanic"],
-  income: "45k",
+  income: "45k USD+",
 };
 
 export const incomeData = [
   {
+    amount: "10k~25k USD",
+  },
+  {
     amount: "25k~30k USD",
   },
   {
-    amount: "30k~40k USD",
+    amount: "30k~45k USD",
   },
   {
     amount: "45k USD+",
